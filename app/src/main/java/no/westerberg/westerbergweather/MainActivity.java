5051edfe99ba9cbb8dd3b962c1c6b6d1b29b5cf0
Thomas Qvidahl <tp@live.no>
2017-03-13 22:03:06 +0100

    Initial commit

---
+++ app/src/main/java/no/westerberg/westerbergweather/MainActivity.java
@@ -0,0 +1,13 @@
+package no.westerberg.westerbergweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
