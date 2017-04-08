80b9712099fe2656e688e37d558bcf05a0a934fa
Thomas Qvidahl <tp@live.no>
2017-04-08 18:55:02 +0200

    Lagt til annotations i XML-klassene Time og WeatherData.

--- app/src/main/java/Time.java
+++ app/src/main/java/Time.java
@@ -1,6 +1,28 @@
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
 /**
- * Created by eline on 08.04.2017.
+ * Created by Eline Westerberg 08.04.2017.
  */
 
+@Root(strict = false)
 public class Time {
+
+    @Attribute
+    String from;
+    @Attribute
+    String to;
+    @Element
+    Symbol symbol;
+    @Element
+    WindDirection windDirection;
+    @Element
+    WindSpeed windSpeed;
+    @Element
+    Temperature temperature;
+    @Element
+    Pressure pressure;
+
+    //TODO Getters and setters
 }
--- app/src/main/java/WeatherData.java
+++ app/src/main/java/WeatherData.java
@@ -1,7 +1,15 @@
-/**
- * Created by eline on 08.04.2017.
- */
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
 
+/**
+ * Created  by Eline Westerberg on 08.04.2017.
+ */
+@Root(strict = false)
 public class WeatherData {
+
+    @Element
+    Forecast forecast;
+
+    //TODO Getters and setters
 }
 
