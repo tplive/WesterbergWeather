e9579396af933eddb775e7d8cb34329fe22cb1ce
Thomas Qvidahl <tp@live.no>
2017-04-09 14:11:40 +0200

    Binding av tekstfelter i fragment_weather i WeatherFragment.java. Også værikon-font.

--- app/src/main/java/layout/WeatherFragment.java
+++ app/src/main/java/layout/WeatherFragment.java
@@ -1,12 +1,15 @@
 package layout;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import no.westerberg.westerbergweather.R;
 
@@ -19,53 +22,37 @@ import no.westerberg.westerbergweather.R;
  * create an instance of this fragment.
  */
 public class WeatherFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Typeface weatherFont;
+
+    TextView cityField;
+    TextView updatedField;
+    TextView detailsField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
+
+    Handler handler;
+
 
     private OnFragmentInteractionListener mListener;
 
     public WeatherFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WeatherFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WeatherFragment newInstance(String param1, String param2) {
-        WeatherFragment fragment = new WeatherFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        handler = new Handler();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_weather, container, false);
+        cityField = (TextView) rootView.findViewById(R.id.cityField);
+        updatedField = (TextView) rootView.findViewById(R.id.updatedField);
+        detailsField = (TextView) rootView.findViewById(R.id.detailsField);
+        currentTemperatureField = (TextView) rootView.findViewById(R.id.temperatureField);
+        weatherIcon = (TextView) rootView.findViewById(R.id.weatherIconField);
+
+        weatherIcon.setTypeface(weatherFont);
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
