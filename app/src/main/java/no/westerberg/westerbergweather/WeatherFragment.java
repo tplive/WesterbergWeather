008b4faabd6e2f7490eb5c9692ef6f7df1014202
Thomas Qvidahl <tp@live.no>
2017-05-19 00:32:06 +0200

    Implementing more from tutsplus. Actual results in view. Crying now. Sobbing, actually. Can go to bed now, and sleep peacefully.

--- app/src/main/java/no/westerberg/westerbergweather/WeatherFragment.java
+++ app/src/main/java/no/westerberg/westerbergweather/WeatherFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import no.westerberg.westerbergweather.CityPreference;
 import no.westerberg.westerbergweather.R;
@@ -60,9 +65,76 @@ public class WeatherFragment extends Fragment {
                                     Toast.LENGTH_LONG).show();
                         }
                     });
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderWeather(json);
+                        }
+                    });
                 }
             }
         }.start();
+    }
+
+    private void renderWeather(JSONObject json) {
+
+        try{
+            cityField.setText(json.getString("name").toUpperCase(Locale.US) +
+            ", " + json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + "hPa"
+            );
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp")) + "˚C");
+
+            DateFormat df = DateFormat.getDateInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+            updatedField.setText("Last updated: " + updatedOn);
+
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
+
+        }catch (Exception e) {
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+        }
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+
+        int id = actualId / 100;
+        String icon = "";
+        if (actualId==800) {
+            long currentTime = new Date().getTime();
+            if(currentTime>=sunrise && currentTime<sunset) {
+                icon = getActivity().getString(R.string.weather_sunny);
+            }else {
+                icon = getActivity().getString(R.string.weather_clear_night);
+            }
+        }else {
+            switch(id) {
+                case 2 : icon = getActivity().getString(R.string.weather_thunder);
+                    break;
+                case 3 : icon = getActivity().getString(R.string.weather_drizzle);
+                    break;
+                case 7 : icon = getActivity().getString(R.string.weather_foggy);
+                    break;
+                case 8 : icon = getActivity().getString(R.string.weather_cloudy);
+                    break;
+                case 6 : icon = getActivity().getString(R.string.weather_snowy);
+                    break;
+                case 5 : icon = getActivity().getString(R.string.weather_rainy);
+                    break;
+            }
+        }
+        weatherIcon.setText(icon);
     }
 
     @Override
